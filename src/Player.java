8fca54ee95cf383e0d1c0c3469260d94dfaefb89
Samuel Piedagnel <samuel.piedagnel@googlemail.com>
2018-04-28 07:14:13 +0100

    commit after reaching silver league

--- src/Player.java
+++ src/Player.java
@@ -5,35 +5,36 @@ import java.util.function.Predicate;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
- 
-class Point{
-    
-    int x;
-    int y;
-    int radius;
-    
-    Point(int x, int y, int radius){
+
+class Point {
+
+    final int x;
+    final int y;
+    final int radius;
+
+    Point(int x, int y, int radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
     }
-    
-    int distance2(Point p){
+
+    int distance2(Point p) {
         return (this.x - p.x) * (this.x - p.x) + (this.y - p.y) * (this.y - p.y);
     }
 }
 
 class Site extends Point {
     private int structureType;
-    private int id;
+    private final int id;
     private int owner;
     private int creepType;
     private int param1;
     private int param2;
+    private int gold;
+    private int maxMineSize;
 
-    Site(int x, int y, int id, int type, int radius) {
+    Site(int x, int y, int id,  int radius) {
         super(x, y, radius);
-        this.structureType = type;
         this.id = id;
     }
 
@@ -65,11 +66,17 @@ class Site extends Point {
         return param2;
     }
 
-    void update(int structureType, int owner, int param1, int param2) {
+    public int getMaxMineSize() {
+        return maxMineSize;
+    }
+
+    void update(int structureType, int owner, int param1, int param2, int gold, int maxMineSize) {
         this.structureType = structureType;
         this.owner = owner;
         this.param1 = param1;
         this.param2 = param2;
+        this.gold = gold;
+        this.maxMineSize = maxMineSize;
     }
 
     int getOwner() {
@@ -77,7 +84,7 @@ class Site extends Point {
     }
 
     @Override
-    public String  toString() {
+    public String toString() {
         return "Site{" +
                 "x=" + x +
                 ", y=" + y +
@@ -88,38 +95,46 @@ class Site extends Point {
                 ", creepType=" + creepType +
                 '}';
     }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
 }
 
-class Unit extends Point{
+class Unit extends Point {
 
-    private int owner;
-    private int type;
-    private int health;
+    private final int owner;
+    private final int type;
+    private final int health;
 
     Unit(int x, int y, int owner, int type, int health) {
         super(x, y, 30);
         this.owner = owner;
         this.type = type;
-        this. health = health;
+        this.health = health;
     }
 
-    boolean isMine(){
+    boolean isMine() {
         return this.owner == 0;
     }
 
-    boolean isQueen(){
+    boolean isQueen() {
         return this.type == -1;
     }
 
-    int getType(){
+    int getType() {
         return this.type;
     }
 }
 
 class Player {
-    
+
     private Site[] sites;
-    private ArrayList<Unit> units = new ArrayList<>();
+    private final ArrayList<Unit> units = new ArrayList<>();
     private Unit myQueen;
     private Unit ennemyQueen;
 
@@ -147,58 +162,64 @@ class Player {
     private final int STRUCTURE_TYPE_TOWER = 1;
     private final int STRUCTURE_TYPE_BARRACK = 2;
     private boolean hasIncreasedRate;
+    private boolean startTrain;
 
 
-    private void setSites(Site[] sites){
+    private void setSites(Site[] sites) {
         this.sites = sites;
     }
 
-    private void updateSite(int siteId, int structureType, int owner, int param1, int param2){
+    private void updateSite(int siteId, int structureType, int owner, int param1, int param2, int gold, int maxMineSize) {
         Site site = this.sites[siteId];
-        site.update(structureType, owner, param1, param2);
-        if(site.getOwner() == OWNER_FRIENDLY){
-            if(site.getStructureType() == STRUCTURE_TYPE_BARRACK) {
+        site.update(structureType, owner, param1, param2, gold, maxMineSize);
+        if (site.getOwner() == OWNER_FRIENDLY) {
+            if (site.getStructureType() == STRUCTURE_TYPE_BARRACK) {
                 if (site.getCreepType() == KNIGHT)
                     nbKnightsSites++;
                 else if (site.getCreepType() == ARCHER)
                     nbArchersSites++;
                 else if (site.getCreepType() == GIANT)
                     nbGiantsSites++;
-            }else if (site.getStructureType()==STRUCTURE_TYPE_MINE){
-                nbMines ++;
-            }else
+            } else if (site.getStructureType() == STRUCTURE_TYPE_MINE) {
+                nbMines++;
+            } else
                 nbTowers++;
-        }else if (site.getStructureType()==STRUCTURE_TYPE_MINE){
-                nbEnnemyMines ++;
+        } else if (site.getStructureType() == STRUCTURE_TYPE_MINE) {
+            nbEnnemyMines++;
         }
     }
 
-    private void setGold(int gold){
+    private void setGold(int gold) {
         this.gold = gold;
+        if (gold > 300)
+            startTrain = true;
     }
 
-    private void addUnit(Unit u){
+    private void addUnit(Unit u) {
         this.units.add(u);
-        if(u.isQueen()) {
-            if(u.isMine())
+        if (u.isQueen()) {
+            if (u.isMine())
                 myQueen = u;
             else
                 ennemyQueen = u;
-        }else if (u.isMine()){
-            if(u.getType() == ARCHER)
-                nbArchers ++;
-            else if(u.getType() == KNIGHT)
-                nbKnights ++;
-            else if(u.getType() == GIANT)
-                nbGiants ++;
+        } else if (u.isMine()) {
+            if (u.getType() == ARCHER)
+                nbArchers++;
+            else if (u.getType() == KNIGHT)
+                nbKnights++;
+            else if (u.getType() == GIANT)
+                nbGiants++;
         }
     }
 
-    private Site findClosestSiteFromQueen(Unit queen, Predicate<Site> p){
+    private Site findClosestSiteFromQueen(Unit queen, Predicate<Site> p) {
+        System.err.println("here2");
         Site closest = null;
         int dist = Integer.MAX_VALUE;
+
         for (Site site : sites) {
             int i1 = queen.distance2(site);
+            System.err.println(site+" "+i1);
             if (i1 < dist && p.test(site)) {
                 closest = site;
                 dist = i1;
@@ -207,7 +228,7 @@ class Player {
         return closest;
     }
 
-    private Site findMostDistantSiteFromQueen(Unit queen, Predicate<Site> p){
+    private Site findMostDistantSiteFromQueen(Unit queen, Predicate<Site> p) {
         Site closest = null;
         int dist = 0;
         for (Site site : sites) {
@@ -220,74 +241,107 @@ class Player {
         return closest;
     }
 
-    private void printQueenAction(){
-        if(this.touchedSite != -1  && sites[touchedSite].getOwner()!= OWNER_FRIENDLY ) {
-            if (nbGiantsSites == 0 ) {
-                System.out.println("BUILD " + touchedSite + " BARRACKS-GIANT");
-                sites[touchedSite].setCreepType(GIANT);
-            }else if(nbKnightsSites == 0){
+    private void printQueenAction() {
+        if (this.touchedSite != -1 && sites[touchedSite].getOwner() != OWNER_FRIENDLY) {
+//            if (nbGiantsSites == 0) {
+//                System.out.println("BUILD " + touchedSite + " BARRACKS-GIANT");
+//                sites[touchedSite].setCreepType(GIANT);
+//            } else
+                if (nbKnightsSites == 0) {
                 System.out.println("BUILD " + touchedSite + " BARRACKS-KNIGHT");
                 sites[touchedSite].setCreepType(KNIGHT);
-            } else {
-                if (nbMines < nbTowers) {
+           } else  if (nbMines == 0) {
                     System.out.println("BUILD " + touchedSite + " MINE");
                     hasIncreasedRate = false;
-                }else
+                } else
                     System.out.println("BUILD " + touchedSite + " TOWER");
-            }
-        }else if(this.touchedSite != -1 && sites[touchedSite].getStructureType() == STRUCTURE_TYPE_MINE && !hasIncreasedRate) {
+     //       }
+        } else if (this.touchedSite != -1 && sites[touchedSite].getStructureType() == STRUCTURE_TYPE_MINE && !hasIncreasedRate) {
             System.out.println("BUILD " + touchedSite + " MINE");
             hasIncreasedRate = true;
-        }else if(this.touchedSite != -1 && sites[touchedSite].getStructureType() == STRUCTURE_TYPE_TOWER &&sites[touchedSite].getParam1() < 700 ){
-                System.out.println("BUILD " + touchedSite + " TOWER");
-                hasIncreasedRate = true;
-        }else{
-           // if(nbArchersSites==0 || nbKnightsSites==0) {
-           if(nbMines>nbEnnemyMines){
-                Site dest = findMostDistantSiteFromQueen(ennemyQueen, s -> s.getOwner() == OWNER_FRIENDLY && s.getStructureType() == STRUCTURE_TYPE_TOWER);
+        } else if (this.touchedSite != -1 && sites[touchedSite].getStructureType() == STRUCTURE_TYPE_TOWER && sites[touchedSite].getParam1() < 700) {
+            System.out.println("BUILD " + touchedSite + " TOWER");
+            hasIncreasedRate = true;
+        } else {
+            // if(nbArchersSites==0 || nbKnightsSites==0) {
+            if (nbTowers == 3) {
+                Site closest = null;
+                int dist = Integer.MAX_VALUE;
+                for (Site site : sites) {
+
+                    if (site.getParam1() < dist && site.getOwner() == OWNER_FRIENDLY && (site.getStructureType() == STRUCTURE_TYPE_TOWER)) {
+                        closest = site;
+                        dist = site.getParam1();
+                    }
+                }
+                Site dest = closest;
+               // Site dest = findMostDistantSiteFromQueen(ennemyQueen, s -> (s.getOwner() == OWNER_FRIENDLY) && (s.getStructureType() == STRUCTURE_TYPE_TOWER));
                 if (dest != null) {
                     System.out.println("MOVE " + (dest.x) + " " + (dest.y));
                     return;
                 }
-           }
-                Site dest = findClosestSiteFromQueen(myQueen, s -> s.getOwner() != OWNER_FRIENDLY && s.getStructureType() != STRUCTURE_TYPE_TOWER);
+            }
+           
+            Site closest = null;
+            long dist = Long.MAX_VALUE;
+            for (Site site : sites) {
+                long i1 = myQueen.distance2(site);
+                if (i1 < dist && (site.getOwner() != OWNER_FRIENDLY) && (site.getId() != touchedSite)) {
+                    closest = site;
+                    dist = i1;
+                }
+            }
+           // Site dest = findClosestSiteFromQueen(myQueen, s -> (s.getOwner() != OWNER_FRIENDLY) && (s.getId() != touchedSite));
+            Site dest= closest;
+            System.err.println("dest is null" + (dest==null));
+            if (dest != null) {
                 System.out.println("MOVE " + (dest.x) + " " + (dest.y));
-
-//            }else{
-//                Site dest = findClosestSiteFromQueen(myQueen, s -> s.getOwner() == 0 && s.getCreepType() == 1);
-//                System.out.println("MOVE " + (dest.x) + " " + (dest.y));
-//            }
+            }else{
+                System.out.println("WAIT");
+            }
         }
     }
 
-    private void resetUnits(){
+    private void resetUnits() {
         this.units.clear();
-        nbKnights  = 0;
-        nbKnightsSites  = 0;
-        nbGiantsSites  = 0;
+        nbKnights = 0;
+        nbKnightsSites = 0;
+        nbGiantsSites = 0;
         nbArchers = 0;
         nbGiants = 0;
         nbArchersSites = 0;
-        nbMines=0;
-        nbEnnemyMines=0;
-        nbTowers=0;
+        nbMines = 0;
+        nbEnnemyMines = 0;
+        nbTowers = 0;
     }
 
     private void printTrainingAction() {
-        boolean actionTaken;
-        System.err.println(nbArchers+" "+nbKnights);
-        if(nbGiants==0){
-            actionTaken = printTrainingGiants();
-        }else{
-            actionTaken = printTrainingKnights();
-       }
-        if(!actionTaken)
+        boolean actionTaken = false;
+      //  if (startTrain) {
+           // if (nbGiants <= 1) {
+           //     actionTaken = printTrainingGiants();
+           // } else {
+                actionTaken = printTrainingKnights();
+          //  }
+        //}
+        if (!actionTaken) {
             System.out.println("TRAIN");
+        }
     }
 
     private boolean printTrainingKnights() {
-        Site site = findClosestSiteFromQueen(ennemyQueen, s -> s.getOwner() == OWNER_FRIENDLY && s.getStructureType() == STRUCTURE_TYPE_BARRACK && s.getCreepType() == KNIGHT );
-        if (site !=null) {
+        Site closest = null;
+        long dist = Long.MAX_VALUE;
+        for (Site site : sites) {
+            long i1 = myQueen.distance2(site);
+            if (i1 < dist && (site.getOwner() == OWNER_FRIENDLY && site.getStructureType() == STRUCTURE_TYPE_BARRACK && site.getCreepType() == KNIGHT)) {
+                closest = site;
+                dist = i1;
+            }
+        }
+        Site site =closest;
+        //Site site = findClosestSiteFromQueen(ennemyQueen, s -> s.getOwner() == OWNER_FRIENDLY && s.getStructureType() == STRUCTURE_TYPE_BARRACK && s.getCreepType() == KNIGHT);
+        if (site != null) {
             System.out.println("TRAIN " + site.getId());
             return true;
         }
@@ -295,8 +349,8 @@ class Player {
     }
 
     private boolean printTrainingGiants() {
-        Site site = findClosestSiteFromQueen(ennemyQueen, s -> s.getOwner() == OWNER_FRIENDLY && s.getStructureType() == STRUCTURE_TYPE_BARRACK && s.getCreepType() == GIANT );
-        if (site !=null) {
+        Site site = findClosestSiteFromQueen(ennemyQueen, s -> s.getOwner() == OWNER_FRIENDLY && s.getStructureType() == STRUCTURE_TYPE_BARRACK && s.getCreepType() == GIANT);
+        if (site != null) {
             System.out.println("TRAIN " + site.getId());
             return true;
         }
@@ -304,8 +358,8 @@ class Player {
     }
 
     private boolean printTrainingArchers() {
-        Site site = findClosestSiteFromQueen(myQueen, s -> s.getOwner() == OWNER_FRIENDLY && s.getCreepType() == ARCHER );
-        if (site !=null) {
+        Site site = findClosestSiteFromQueen(myQueen, s -> s.getOwner() == OWNER_FRIENDLY && s.getCreepType() == ARCHER);
+        if (site != null) {
             System.out.println("TRAIN " + site.getId());
             return true;
         }
@@ -325,19 +379,17 @@ class Player {
         Site[] sites = new Site[numSites];
         for (int i = 0; i < numSites; i++) {
             int siteId = in.nextInt();
-             int x = in.nextInt();
+            int x = in.nextInt();
             int y = in.nextInt();
             int radius = in.nextInt();
-            sites[siteId] = new Site(x, y, siteId, -1, radius);
+            sites[siteId] = new Site(x, y, siteId, radius);
         }
         p.setSites(sites);
 
-        int structureType = -1;
-        int x = 0;
-        int y =0;
-        int siteId = 0;
-        int touchedSite = 0;
-        int trainingSite =0;
+        int x;
+        int y;
+        int siteId;
+        int touchedSite ;
         // game loop
         while (true) {
             int gold = in.nextInt();
@@ -347,13 +399,13 @@ class Player {
             p.setTouchedSite(touchedSite);
             for (int i = 0; i < numSites; i++) {
                 siteId = in.nextInt();
-                int ignore1 = in.nextInt(); // used in future leagues
-                int ignore2 = in.nextInt(); // used in future leagues
-                structureType = in.nextInt(); // -1 = No structure, 2 = Barracks
+                int mineGold = in.nextInt(); // used in future leagues
+                int maxMineSize = in.nextInt(); // used in future leagues
+                int structureType = in.nextInt(); // -1 = No structure, 2 = Barracks
                 int owner = in.nextInt(); // -1 = No structure, 0 = Friendly, 1 = Enemy
                 int param1 = in.nextInt();
                 int param2 = in.nextInt();
-                p.updateSite(siteId, structureType, owner, param1, param2);
+                p.updateSite(siteId, structureType, owner, param1, param2, mineGold, maxMineSize);
             }
             int numUnits = in.nextInt();
             for (int i = 0; i < numUnits; i++) {
